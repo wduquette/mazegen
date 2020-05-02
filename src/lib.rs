5569873f3cd630b24cad43c2665a8bbd7d543f76
Will Duquette <will@wjduquette.com>
2020-05-02 11:50:46 -0700

    Improved grid implementation.

--- src/lib.rs
+++ src/lib.rs
@@ -9,8 +9,12 @@ pub fn hello() {
 pub struct Grid {
     num_rows: usize,
     num_cols: usize,
-    cells: Vec<Cell>,
+    num_cells: usize,
+    cells: Vec<CellData>,
 }
+
+// A Cell is an index into the cells vector
+pub type Cell = usize;
 
 /// TODO: Consider making this generic.  The link information is wholly contained in
 /// the Grid structure; but there's a generic data record for each cell so that the
@@ -18,171 +22,322 @@ pub struct Grid {
 impl Grid {
     pub fn new(num_rows: usize, num_cols: usize) -> Self {
         // FIRST, initialize the cells vector
-        let mut cells = Vec::with_capacity(num_rows * num_cols);
+        let num_cells = num_rows * num_cols;
+        let cells = Vec::with_capacity(num_cells);
 
-        for i in 0..num_rows {
-            for j in 0..num_cols {
-                cells.push(Cell::new(i, j));
-            }
-        }
-
-        // NEXT, return the grid
-        Self {
+        let mut grid = Self {
             num_rows,
             num_cols,
+            num_cells,
             cells,
+        };
+
+        for cell in 0..num_cells {
+            let i = grid.i(cell);
+            let j = grid.j(cell);
+
+            let north = if i > 0 {
+                Some(dbg!(grid.cell(i - 1,j)))
+            } else {
+                None
+            };
+
+            let south = if i < num_rows - 1 {
+                Some(grid.cell(i + 1, j))
+            } else {
+                None
+            };
+
+            let east = if j < num_cols - 1 {
+                Some(grid.cell(i, j + 1))
+            } else {
+                None
+            };
+
+            let west = if j > 0 {
+                Some(grid.cell(i, j - 1))
+            } else {
+                None
+            };
+
+            grid.cells.push(CellData {
+                cell,
+                links: HashSet::new(),
+                north,
+                south,
+                east,
+                west,
+            });
         }
+
+        grid
     }
 
+    /// The number of rows in the grid.
     pub fn num_rows(&self) -> usize {
         self.num_rows
     }
 
+    /// The number of columns in the grid.
     pub fn num_cols(&self) -> usize {
         self.num_cols
     }
 
-    // Links location 1 to location 2
-    pub fn link(&mut self, loc1: Loc, loc2: Loc) {
-        let index1 = self.index(loc1);
-        let index2 = self.index(loc2);
-
-        self.cells[index1].link(loc2);
-        self.cells[index2].link(loc1);
+    /// The number of cells in the grid.
+    pub fn num_cells(&self) -> usize {
+        self.num_cells
     }
 
-    // Unlinks location 1 from location 2
-    pub fn unlink(&mut self, loc1: Loc, loc2: Loc) {
-        let index1 = self.index(loc1);
-        let index2 = self.index(loc2);
-
-        self.cells[index1].unlink(loc2);
-        self.cells[index2].unlink(loc1);
+    /// Computes the cell from the row and column.
+    pub fn cell(&self, i: usize, j: usize) -> Cell {
+        assert!(i < self.num_rows && j < self.num_cols);
+        i * self.num_cols + j
     }
 
-    // Gets a reference to the cell at the given location
-    pub fn cell(&self, loc: Loc) -> Option<&Cell> {
-        if  self.contains(loc) {
-            Some(&self.cells[self.index(loc)])
-        } else {
-            None
-        }
+    /// Computes the row index from the cell ID.
+    pub fn i(&self, cell: Cell) -> usize {
+        assert!(self.contains(cell));
+        cell / self.num_cols
+    }
+
+    /// Computes the column index from the cell ID.
+    pub fn j(&self, cell: Cell) -> usize {
+        assert!(self.contains(cell));
+        cell % self.num_cols
+    }
+
+    // Links cell 1 to cell 2.
+    // The presumption is that the cells are adjacent.
+    pub fn link(&mut self, cell1: Cell, cell2: Cell) {
+        assert!(self.contains(cell1));
+        assert!(self.contains(cell2));
+
+        self.cells[cell1].link(cell2);
+        self.cells[cell2].link(cell1);
+    }
+
+    // Unlinks cell 1 from cell 2
+    pub fn unlink(&mut self, cell1: Cell, cell2: Cell) {
+        self.cells[cell1].unlink(cell2);
+        self.cells[cell2].unlink(cell1);
+    }
+
+    // Gets the cells linked to this cell
+    pub fn links(&self, cell: Cell) -> Vec<Cell> {
+        assert!(self.contains(cell));
+        self.cells[cell].links.iter().copied().collect()
+    }
+
+    // Indicates whether or not the cells are linked
+    pub fn is_linked(&self, cell1: Cell, cell2: Cell) -> bool {
+        assert!(self.contains(cell1));
+        assert!(self.contains(cell2));
+
+        self.cells[cell1].links.contains(&cell2)
+    }
+
+    // Gets the neighbors to the north, south, east, and west of this cell.
+    pub fn neighbors(&self, cell: Cell) -> Vec<Cell> {
+        assert!(self.contains(cell));
+        self.cells[cell].neighbors()
     }
 
     /// Does the grid contain the location?
-    pub fn contains(&self, loc: Loc) -> bool {
+    pub fn contains(&self, cell: Cell) -> bool {
         // NOTE: No need to check against zero, since we're using an unsigned type.
-        loc.row() < self.num_rows && loc.col() < self.num_cols
+        cell < self.num_cells
     }
 
-    /// Returns the index of the given location in the cells vector.
-    fn index(&self, loc: Loc) -> usize {
-        loc.row()*self.num_rows + loc.col()
+    /// Gets the cell to the north, if any.
+    pub fn north_of(&self, cell: Cell) -> Option<Cell> {
+        assert!(self.contains(cell));
+        self.cells[cell].north
+    }
+
+    /// Gets the cell to the south, if any.
+    pub fn south_of(&self, cell: Cell) -> Option<Cell> {
+        assert!(self.contains(cell));
+        self.cells[cell].south
+    }
+
+    /// Gets the cell to the east, if any.
+    pub fn east_of(&self, cell: Cell) -> Option<Cell> {
+        assert!(self.contains(cell));
+        self.cells[cell].east
+    }
+
+    /// Gets the cell to the west, if any.
+    pub fn west_of(&self, cell: Cell) -> Option<Cell> {
+        assert!(self.contains(cell));
+        self.cells[cell].west
     }
 }
 
 impl Display for Grid {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        write!(f, "Grid({}x{})", self.num_rows, self.num_cols)
+        write!(f, "Grid({}x{}={})", self.num_rows, self.num_cols, self.num_cells)
+        // TODO
     }
 }
-
-/// New type: the coordinates of a maze cell.
-/// TODO: could probably use a smaller integer type.
-/// TODO: could possibly use a signed type, so that mazes can grow in all directions.
-/// If I do that, though, I'll need a different data structure in Grid.
-#[derive(Debug,PartialEq,Eq,Copy,Clone,Hash)]
-pub struct Loc {
-    row: usize,
-    col: usize,
-}
-
-impl Loc {
-    pub fn new(row: usize, col: usize) -> Self {
-        Self { row, col }
-    }
-    pub fn row(&self) -> usize {
-        self.row
-    }
-
-    pub fn col(&self) -> usize {
-        self.col
-    }
-
-    /// Returns the location to the north
-    pub fn north(&self) -> Loc {
-        // TODO: Problem: if in top row, there is no valid location to the north because
-        // the row is negative and the variable is unsigned
-        Loc::new(self.row - 1, self.col)
-    }
-
-    /// Returns the location to the south
-    pub fn south(&self) -> Loc {
-        Loc::new(self.row + 1, self.col)
-    }
-
-    /// Returns the location to the east
-    pub fn east(&self) -> Loc {
-        Loc::new(self.row, self.col + 1)
-    }
-
-    /// Returns the location to the west
-    pub fn west(&self) -> Loc {
-        // TODO: Problem: if in leftmost column, there is no valid location to the west because
-        // the column is negative and the variable is unsigned
-        Loc::new(self.row, self.col - 1)
-    }
-}
-
-
 
 #[derive(Debug,PartialEq,Eq,Clone)]
-pub struct Cell {
-    loc: Loc,
-    links: HashSet<Loc>,
+struct CellData {
+    cell: Cell,
+    links: HashSet<Cell>,
+    north: Option<Cell>,
+    south: Option<Cell>,
+    east: Option<Cell>,
+    west: Option<Cell>,
 }
 
-impl Cell {
-    /// Creates a new Cell
-    pub fn new(row: usize, col: usize) -> Self {
-        Self {
-            loc: Loc::new(row, col),
-            links: HashSet::new(),
-        }
-    }
-
-    pub fn loc(&self) -> Loc {
-        self.loc
-    }
-
-    /// Returns the cell's row index
-    pub fn row(&self) -> usize {
-        self.loc.row()
-    }
-
-    /// Returns the cell's column index
-    pub fn col(&self) -> usize {
-        self.loc.col()
-    }
-
-    /// Inserts a link to the given location
-    fn link(&mut self, other: Loc) {
+impl CellData {
+    /// Inserts a link to the given
+    fn link(&mut self, other: Cell) {
         self.links.insert(other);
     }
 
-    /// Removes a link to the given location
-    fn unlink(&mut self, other: Loc) {
+    /// Removes a link to the given cell
+    fn unlink(&mut self, other: Cell) {
         self.links.remove(&other);
     }
 
-    /// Returns an interator over the links
-    pub fn links(&self) -> std::collections::hash_set::Iter<Loc> {
-        self.links.iter()
+    fn neighbors(&self) -> Vec<Cell> {
+        let mut vec = Vec::new();
+
+        if let Some(cell) = self.north {
+            vec.push(cell)
+        }
+
+        if let Some(cell) = self.south {
+            vec.push(cell)
+        }
+
+        if let Some(cell) = self.east {
+            vec.push(cell)
+        }
+
+        if let Some(cell) = self.west {
+            vec.push(cell)
+        }
+
+        vec
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_grid_new() {
+        let grid = Grid::new(5, 6);
+
+        assert_eq!(grid.num_rows(), 5);
+        assert_eq!(grid.num_cols(), 6);
+        assert_eq!(grid.num_cells(), 30);
     }
 
-    /// Indicates whether or not this cell is linked to another
-    pub fn linked(&self, loc: Loc) -> bool {
-        self.links.contains(&loc)
+    #[test]
+    fn test_grid_cell() {
+        let grid = Grid::new(5,6);
+
+        assert_eq!(grid.cell(0,0), 0);
+        assert_eq!(grid.cell(0,3), 3);
+        assert_eq!(grid.cell(1,0), 6);
+        assert_eq!(grid.cell(1,3), 9);
+        assert_eq!(grid.cell(2,0), 12);
+        assert_eq!(grid.cell(4,5), grid.num_cells() - 1);
     }
 
+    #[test]
+    fn test_grid_i_j() {
+        let grid = Grid::new(5,6);
+
+        assert_eq!(grid.i(0), 0);
+        assert_eq!(grid.j(0), 0);
+
+        assert_eq!(grid.i(3), 0);
+        assert_eq!(grid.j(3), 3);
+
+        assert_eq!(grid.i(6), 1);
+        assert_eq!(grid.j(6), 0);
+
+        assert_eq!(grid.i(9), 1);
+        assert_eq!(grid.j(9), 3);
+    }
+
+    #[test]
+    fn test_grid_cell_i_j() {
+        let grid = Grid::new(5,6);
+
+        for i in 0..grid.num_rows() {
+            for j in 0..grid.num_cols() {
+                let cell = grid.cell(i, j);
+                assert!(grid.contains(cell));
+                assert_eq!(grid.i(cell), i);
+                assert_eq!(grid.j(cell), j);
+            }
+        }
+    }
+
+    #[test]
+    fn test_grid_neighbors() {
+        let grid = Grid::new(5,6);
+
+        for cell in 0..grid.num_cells() {
+            let mut count = 0;
+            let neighbors = grid.neighbors(cell);
+
+            if let Some(other) = grid.north_of(cell) {
+                count += 1;
+                assert!(grid.contains(other));
+                assert!(neighbors.contains(&other));
+            }
+
+            if let Some(other) = grid.south_of(cell) {
+                count += 1;
+                assert!(grid.contains(other));
+                assert!(neighbors.contains(&other));
+            }
+
+            if let Some(other) = grid.east_of(cell) {
+                count += 1;
+                assert!(grid.contains(dbg!(other)));
+                assert!(neighbors.contains(&other));
+            }
+
+            if let Some(other) = grid.west_of(cell) {
+                count += 1;
+                assert!(grid.contains(other));
+                assert!(neighbors.contains(&other));
+            }
+
+            assert_eq!(neighbors.len(), count);
+        }
+    }
+
+    #[test]
+    fn test_grid_linking() {
+        let mut grid = Grid::new(5,6);
+
+        // Initially, no cells are linked.
+        for c1 in 0..grid.num_cells {
+            for c2 in 0..grid.num_cells {
+                assert!(!grid.is_linked(c1, c2));
+            }
+        }
+
+        // Link each cell to its eastern neighbor (if any)
+        for c1 in 0..grid.num_cells {
+            if let Some(c2) = grid.east_of(c1) {
+                grid.link(c1, c2);
+                assert!(grid.is_linked(c1, c2));
+                assert!(grid.is_linked(c2, c1));
+
+                grid.unlink(c1, c2);
+                assert!(!grid.is_linked(c1, c2));
+                assert!(!grid.is_linked(c2, c1));
+            }
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,32 @@
 use rand::{thread_rng, Rng};
 use mazegen::Grid;
-use mazegen::Loc;
 
 fn main() {
     let mut grid = Grid::new(10,10);
     binary_tree_maze(&mut grid);
     println!("Made {}", grid);
-
-
 }
 
 fn binary_tree_maze(grid: &mut Grid) {
     // TODO: consider cell iterator
     let mut rng = thread_rng();
 
-    for i in 0..grid.num_rows() {
-        for j in 0..grid.num_cols() {
-            let loc = Loc::new(i, j);
-            let mut neighbors = Vec::new();
+    for cell in 0..grid.num_cells() {
+        let mut neighbors = Vec::new();
 
-            if grid.contains(loc.north()) {
-                neighbors.push(loc.north());
-            }
+        if let Some(ncell) = grid.north_of(cell) {
+            neighbors.push(ncell);
+        }
 
-            if grid.contains(loc.east()) {
-                neighbors.push(loc.east());
-            }
+        if let Some(ecell) = grid.east_of(cell) {
+            neighbors.push(ecell);
+        }
 
-            if neighbors.len() == 2 {
-                let ind: usize = rng.gen_range(0, 2);
-                grid.link(loc, neighbors[ind]);
-            } else if neighbors.len() == 1 {
-                grid.link(loc, neighbors[0]);
-            }
+        if neighbors.len() == 2 {
+            let ind: usize = rng.gen_range(0, 2);
+            grid.link(cell, neighbors[ind]);
+        } else if neighbors.len() == 1 {
+            grid.link(cell, neighbors[0]);
         }
     }
 }
