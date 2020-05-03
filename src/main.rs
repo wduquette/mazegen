635698aa2d768be8444dcddd67f50202bc1cfa72
Will Duquette <will@wjduquette.com>
2020-05-03 09:57:26 -0700

    Refactor `sample()` out of `main`

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use rand::{thread_rng, Rng};
-use mazegen::Grid;
+use mazegen::{Grid, Cell};
 
 fn main() {
     let mut grid = Grid::new(10,10);
@@ -8,9 +8,6 @@ fn main() {
 }
 
 fn binary_tree_maze(grid: &mut Grid) {
-    // TODO: consider cell iterator
-    let mut rng = thread_rng();
-
     for cell in 0..grid.num_cells() {
         let mut neighbors = Vec::new();
 
@@ -22,11 +19,20 @@ fn binary_tree_maze(grid: &mut Grid) {
             neighbors.push(ecell);
         }
 
-        if neighbors.len() == 2 {
-            let ind: usize = rng.gen_range(0, 2);
-            grid.link(cell, neighbors[ind]);
-        } else if neighbors.len() == 1 {
-            grid.link(cell, neighbors[0]);
+        if !neighbors.is_empty() {
+            grid.link(cell, sample(&neighbors));
         }
     }
+}
+
+fn sample(vec: &[Cell]) -> Cell {
+    assert!(!vec.is_empty());
+
+    if vec.len() == 1 {
+        return vec[0];
+    }
+
+    let mut rng = thread_rng();
+    let ind: usize = rng.gen_range(0, vec.len());
+    vec[ind]
 }
