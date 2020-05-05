8b44d3661dcc6e840ea9db1ea40899b8d0e5f9a8
Will Duquette <will@wjduquette.com>
2020-05-04 19:10:26 -0700

    Added simple "grid" command and object.

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,7 @@ fn main() {
     // NEXT, initialize the interpreter
     let mut interp = Interp::new();
     interp.add_command("doit", cmd_doit);
+    interp.add_command("grid", cmd_grid);
 
     // Install a Molt extension crate
     // molt_sample::install(&mut interp).expect("Could not install.");
@@ -35,4 +36,49 @@ pub fn cmd_doit(_interp: &mut Interp,  _: ContextID, argv: &[Value]) -> MoltResu
     image.save("temp.png").unwrap();
 
     molt_ok!(grid.to_string())
+}
+
+pub fn cmd_grid(interp: &mut Interp, _: ContextID, argv: &[Value]) -> MoltResult {
+    // Correct number of arguments?
+    check_args(1, argv, 2, 2, "name")?;
+
+    let name = argv[1].as_str();
+    let grid = Grid::new(10, 20);
+    let ctx = interp.save_context(grid);
+    interp.add_context_command(name, obj_grid, ctx);
+
+    molt_ok!(name)
+}
+
+pub fn obj_grid(interp: &mut Interp, ctx : ContextID, argv: &[Value]) -> MoltResult {
+    interp.call_subcommand(ctx, argv, 1, &GRID_SUBCOMMANDS)
+}
+
+const GRID_SUBCOMMANDS: [Subcommand; 3] = [
+    Subcommand("text", obj_grid_text),
+    Subcommand("rows", obj_grid_rows),
+    Subcommand("cols", obj_grid_cols),
+];
+
+pub fn obj_grid_text(interp: &mut Interp, ctx: ContextID, argv: &[Value]) -> MoltResult {
+    // Correct number of arguments?
+    check_args(2, argv, 2, 2, "")?;
+
+    let grid = interp.context::<Grid>(ctx);
+    molt_ok!(grid.to_string())
+}
+
+pub fn obj_grid_rows(interp: &mut Interp, ctx: ContextID, argv: &[Value]) -> MoltResult {
+    // Correct number of arguments?
+    check_args(2, argv, 2, 2, "")?;
+
+    let grid = interp.context::<Grid>(ctx);
+    molt_ok!(grid.num_rows() as MoltInt)
+}
+
+pub fn obj_grid_cols(interp: &mut Interp, ctx: ContextID, argv: &[Value]) -> MoltResult {
+    // Correct number of arguments?
+    check_args(2, argv, 2, 2, "")?;
+    let grid = interp.context::<Grid>(ctx);
+    molt_ok!(grid.num_cols() as MoltInt)
 }
